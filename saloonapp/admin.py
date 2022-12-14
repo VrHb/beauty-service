aa2e825393be2619e81ef9cf2cf5685505349e3e
KirillYabl <kirill-yablunovskii@mail.ru>
2022-12-14 15:27:41 +0300

    1. Добавил аватар юзеру и админку

--- saloonapp/admin.py
+++ saloonapp/admin.py
@@ -1,23 +1,27 @@
 from django.contrib import admin
 
-from .models import Saloon, Service, ServiceGroup, Master, MasterSpeciality 
+from .models import Saloon, Service, ServiceGroup, Master, MasterSpeciality
 
 
 @admin.register(Saloon)
 class SalonAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(ServiceGroup)
 class ServiceGroupAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Master)
 class MasterAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(MasterSpeciality)
 class MasterSpecialityAdmin(admin.ModelAdmin):
--- userapp/models.py
+++ userapp/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+
+from saloonapp.validators import validate_svg_file_extension
 
 
 class User(AbstractUser):
     phone = PhoneNumberField('контактный номер', region='RU')
+    avatar = models.FileField('заглавное фото салона', validators=[validate_svg_file_extension], null=True, blank=True)
