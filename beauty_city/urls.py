91ac5c6a3f2dd26ca1f3cc82f2f9c47c0dbab149
Victor Abdukarimov <viredmi@gmail.com>
2022-12-14 18:47:19 +0300

    Fixed urls

--- beauty_city/urls.py
+++ beauty_city/urls.py
@@ -1,14 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-from saloonapp import views
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("", include("saloonapp.urls"))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
