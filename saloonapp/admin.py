7f0eca953fa35909352d994aa45230ebfecb1d8d
Victor Abdukarimov <viredmi@gmail.com>
2022-12-14 08:56:18 +0300

    Added base admin fields

--- saloonapp/admin.py
+++ saloonapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Saloon, Service, Master 
+
+
+@admin.register(Saloon)
+class SalonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    pass
