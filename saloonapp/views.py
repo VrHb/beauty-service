d33822ab68533f5a585da261993237b16e999804
Victor Abdukarimov <viredmi@gmail.com>
2022-12-14 17:18:51 +0300

    Added user avatar if having

--- saloonapp/views.py
+++ saloonapp/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 from .models import Saloon
 from .models import Service
 from .models import Master
+from .models import User 
 
 
 def index(request):
     context = {
+        'user': User.objects.get(id=1),
         'saloons': Saloon.objects.all(),
         'services': Service.objects.all(),
         'masters': Master.objects.select_related('speciality').all(),
