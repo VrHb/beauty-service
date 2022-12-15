dfa4a09e2d9a47ce40000f16868da6d16e98b729
Victor Abdukarimov <viredmi@gmail.com>
2022-12-15 11:59:52 +0300

    Added base template for user notes

--- saloonapp/urls.py
+++ saloonapp/urls.py
@@ -10,6 +10,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin-view'),
     path('', views.index, name='main-view'),
+    path('notes/', views.notes, name='notes-view'),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
