849a27227a65e5b64733013480c3e554a475ae3b
KirillYabl <kirill-yablunovskii@mail.ru>
2022-12-17 15:24:16 +0300

    1. Добавил метод для извелечения занятого времени 2. В JS сделал подтягиевание времени при изменении фильтров

--- saloonapp/views.py
+++ saloonapp/views.py
@@ -1,6 +1,8 @@
+from collections import Counter
 from decimal import Decimal
 
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 from django.utils import timezone
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
@@ -10,9 +12,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.shortcuts import render, redirect
 
-from .models import Saloon, ServiceGroup
+from .models import Saloon
+from .models import ServiceGroup
 from .models import Service
 from .models import Master
+from .models import SaloonMasterWeekday
 from .models import Note
 from .serializers import GetFreeTimeslotsSerializer
 from .serializers import SaloonSerializer
@@ -76,6 +80,64 @@ def get_free_timeslots(request: Request):
         serializer.validated_data.get('service', None),
     )
     return Response(calendar)
+
+
+@api_view(['GET'])
+def get_blocked_timeslots(request: Request):
+    serializer = GetFreeTimeslotsSerializer(data=request.query_params)
+    serializer.is_valid(raise_exception=True)
+    timeslots = [f'{t}:00' for t in range(10, 21)]
+
+    # составим фильтры для записей и для рабочих дней мастеров в салонах
+    note_filters = {'date': serializer.validated_data['date']}
+    saloon_master_filters = {'isoweekday': note_filters['date'].isoweekday()}
+    for key in ['saloon', 'service', 'master']:
+        if key in serializer.validated_data:
+            note_filters[f'{key}__pk'] = serializer.validated_data[key]
+            if key == 'service':
+                saloon_master_filters['saloonmaster__master__services__pk__in'] = [serializer.validated_data[key]]
+            else:
+                saloon_master_filters[f'saloonmaster__{key}__pk'] = serializer.validated_data[key]
+
+    # если нет рабочих дней у мастеров в салонах по фильтрам, то все занято
+    saloon_master_day_items = SaloonMasterWeekday.objects.filter(**saloon_master_filters)
+    if not saloon_master_day_items:
+        print('saloon_master_day_items')
+        return Response(timeslots)
+
+    # если среди фильтров только дата, то пользователь все равно
+    # еще будет делать выбор, поэтому отдадим, что все свободно
+    if len(note_filters.keys()) == 1:
+        print('keys')
+        return Response([])
+
+    # если нет записей по фильтрам, то все свободно
+    note_stimes = Note.objects.filter(**note_filters)
+    if not note_stimes:
+        print('note_stimes')
+        return Response([])
+
+    # составляем комбинации [салон, мастер(в котором работает мастер)]
+    # услуга в комбинациях не нужна, т.к. мастер не может делать 2 услуги одновременно,
+    # но по услуге проверятся, что он вообще делает выбранную или любую, если не выбрана
+    combs = set()
+    for saloon_master_day_item in saloon_master_day_items:
+        saloon_pk = saloon_master_day_item.saloonmaster.saloon.pk
+        master_pk = saloon_master_day_item.saloonmaster.master.pk
+        services = saloon_master_day_item.saloonmaster.master.services.all()
+        if 'service__pk' in note_filters:
+            services = services.filter(pk=note_filters['service__pk'])
+        if services:
+            combs.add((saloon_pk, master_pk))
+
+    # и дальше проверяем по каждому часу, если записей на этот час больше
+    # или равно числу возможных комбинаций, то это время занято
+    blocked_times = []
+    note_stimes = note_stimes.values('stime').annotate(cnt=Count('stime'))
+    for note_stime in note_stimes:
+        if note_stime['cnt'] >= len(combs):
+            blocked_times.append(str(note_stime['stime'])[:-3])
+    return Response(blocked_times)
 
 
 def services(request):
